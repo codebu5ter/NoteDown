5fdf94f726cddf0378d76454a3235ab725354bbd
Nikhil Nayak <iamnikhilnayak@outlook.com>
2018-01-12 22:51:15 +0530

    Setup React components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Note from './Note/Note'
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      notes: [
+        { id: 1, noteContent: "Note 1 here!" },
+        { id: 2, noteContent: "Note 2 here!" },
+      ],
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="notesWrapper">
+        <div className="notesHeader">
+          <div className="heading">React & Firebase To-Do List</div>
+        </div>
+        <div className="notesBody">
+          <Note noteContent={} noteId={} key={} />
+        </div>
+        <div className="notesFooter">
+          Footer will go here...
+        </div>
       </div>
     );
   }

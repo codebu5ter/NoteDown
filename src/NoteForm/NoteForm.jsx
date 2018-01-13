3e8570223270c7108442bb1f12a0cd33151a3084
Nikhil Nayak <iamnikhilnayak@outlook.com>
2018-01-13 21:43:15 +0530

    Update NoteForm

--- src/NoteForm/NoteForm.jsx
+++ src/NoteForm/NoteForm.jsx
@@ -5,15 +5,36 @@ class NoteForm extends Component{
 
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {
+      newNoteContent: '',
+    };
+
+    this.handleUserInput = this.handleUserInput.bind(this);
+    this.writeNote = this.writeNote.bind(this);
+  }
+
+  handleUserInput(e){
+    this.setState({
+      newNoteContent: e.target.value,
+    })
+  }
+
+  writeNote(){
+    this.setState({
+      newNoteContent: '',
+    })
   }
 
   render(){
     return(
       <div className="formWrapper">
         <input className="noteInput"
-          placeholder="Write a new note..." />
-        <button className="noteButton">Add Note</button>
+          placeholder="Write a new note..."
+          value={this.state.newNoteContent}
+          onChange={this.handleUserInput} />
+        <button className="noteButton"
+          onClick={this.writeNote}
+        >Add Note</button>
       </div>
     )
   }
